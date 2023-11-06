8fa33e1b3e0c10ee78efe6f8363f7b7e1c490024
Cristian Gomez <svpr0@hotmail.com>
2023-11-06 19:42:46 -0300

    1.0.6
    metascore colored

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -5,16 +5,27 @@ function MovieCard({poster, title, releaseDate, duration, maturity, genres, dire
     <div className="mx-auto max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
       <img src={poster} alt={title} className="w-full h-80" />
       <div className="p-4">
-        <h2 className="text-xl font-semibold">{title}</h2>
-        <p>Año de lanzamiento: {releaseDate}</p>
-        <p>Duración: {duration} minutos</p>
-        <p>Clasificación: {maturity}</p>
-        <p>Géneros: {genres.join(', ')}</p>
-        <p>Director: {director}</p>
-        <p>Actores Principales: {mainActors.join(', ')}</p>
-        <p>Trama: {plot}</p>
-        <p>Metascore: {metascore}</p>
-        <p>Calificación: {rating}</p>
+        <h2 className="text-3xl font-semibold">{title}</h2>
+        <p><b>Año de lanzamiento:</b> {releaseDate}</p>
+        <p><b>Duración:</b> {duration} minutos</p>
+        <p><b>Clasificación:</b> {maturity}</p>
+        <p><b>Géneros:</b> {genres.join(', ')}</p>
+        <p><b>Director:</b> {director}</p>
+        <p><b>Actores Principales:</b> {mainActors.join(', ')}</p>
+        <p><b>Trama:</b> {plot}</p>
+        {(function(){
+              if (metascore > 80) {
+                return (<button className='bg-green-600 text-white font-bold py-1 px-4 rounded'><b>Metascore:</b> {metascore}</button>);
+              } else if (metascore > 60) {
+                return <button className='bg-yellow-400 text-white font-bold py-1 px-4 rounded'><b>Metascore:</b> {metascore}</button>;
+              } else if (metascore > 40) {
+                return <button className='bg-orange-600 text-white font-bold py-1 px-4 rounded'><b>Metascore:</b> {metascore}</button>;
+              } else if (metascore > 20){
+                return <button className='bg-red-600 text-white font-bold py-1 px-4 rounded'><b>Metascore:</b> {metascore}</button>;
+              }
+          }).call(this)
+        }
+        <p><b>Calificación:</b> {rating}</p>
       </div>
     </div>
   );
