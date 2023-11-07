004bdb4f5dd2bb40b1193cf463aedbf4822f5ee3
Cristian Gomez <svpr0@hotmail.com>
2023-11-07 12:13:38 -0300

    Update Formulario.js

--- src/Formulario.js
+++ src/Formulario.js
@@ -4,8 +4,8 @@ class Formulario extends Component {
   constructor() {
     super();
     this.state = {
-      nombre: '',
-      email: '',
+      username: '',
+      password: '',
     };
   }
 
@@ -23,32 +23,39 @@ class Formulario extends Component {
   render() {
     return (
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={this.handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
+            <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+            >
+              Usuario
             </label>
-            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+            <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Usuario"
+            value={this.state.nombre}
+            onChange={this.handleInputChange}
+            />
           </div>
           <div className="mb-6">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
+              Contraseña
             </label>
-            <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-            <p className="text-red-500 text-xs italic">Please choose a password.</p>
+            <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********"/>
+            <p className="text-red-500 text-xs italic">Por favor ingresa una contraseña.</p>
           </div>
           <div className="flex items-center justify-between">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-              Sign In
+              Conectarse
             </button>
             <p className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">
-              Forgot Password?
+              Olvidaste tu contraseña?
             </p>
           </div>
         </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2020 Acme Corp. All rights reserved.
-        </p>
       </div>
       /* <form onSubmit={this.handleSubmit}>
         <div>
