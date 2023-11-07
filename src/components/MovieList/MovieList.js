62091a33e3c3111537578ef83d67a3a46b2a4fd0
Cristian Gomez <svpr0@hotmail.com>
2023-11-07 18:41:11 -0300

    1.2.1
    fix key

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -4,8 +4,8 @@ import MovieCard from '../MovieCard/MovieCard';
 function MovieList({ movies }) {
   return (
     <div className="mb-10 mx-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {movies.map((movie, index) => (
-        <MovieCard key={index} {...movie} />
+      {movies.map((movie) => (
+        <MovieCard key={movie.title+movie.duration} {...movie} />
       ))}
     </div>
   );
