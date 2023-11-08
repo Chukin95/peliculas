eeb59dfd309cb20d9b9e8c85d04e0b803babd0e0
Cristian Gomez <svpr0@hotmail.com>
2023-11-08 16:51:04 -0300

    2.4.0
    NextPrev

--- src/components/MoviesAPI/Movie.js
+++ src/components/MoviesAPI/Movie.js
@@ -5,6 +5,21 @@ import Card from './Card';
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+
+  function handleNextPage() {
+    setPage(page+1);
+    console.log("Pagina número: " + (page+1));
+  }
+
+  function handlePrevPage() {
+    if (page > 1) {
+      setPage(page-1);
+    } else {
+      setPage(1);
+    }
+    console.log("Pagina número: " + (page>1 ? page-1 : 1));
+  }
 
   useEffect(() => {
     axios
@@ -13,7 +28,7 @@ const Movie = () => {
           api_key: '4292278a6986e2bb799d30006b16698d',
           language: 'es-ES',
           sort_by: 'popularity.desc',
-          page: 1,
+          page: page,
         },
       })
       .then((response) => {
@@ -22,10 +37,23 @@ const Movie = () => {
       .catch((error) => {
         console.error('Error al obtener los datos:', error);
       });
-  }, []);
+  }, [page]);
 
   return (
     <div>
+      <div className="inline-flex">
+        <button
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l"
+        onClick={handlePrevPage}>
+          Prev
+        </button>
+        <button
+        className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r"
+        onClick={handleNextPage}
+        >
+          Next
+        </button>
+      </div>
       <div className="my-10 mx-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
         {movies.map((movie) => (
           <Card key={movie.id} movie={movie} />
