5e2356a015e5e2c56eb07aa263b1769406a92bfb
Cristian Gomez <svpr0@hotmail.com>
2023-11-09 19:43:30 -0300

    Update MovieList.js

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -5,7 +5,7 @@ function MovieList({ movies }) {
   return (
     <div className="mb-10 mx-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {movies.map((movie) => (
-        <MovieCard key={movie.title+movie.director} poster={movie.poster} title={movie.title} releaseDate={movie.releaseDate} duration={movie.duration} maturity={movie.maturity} genres={movie.genres} director={movie.director} mainActors={movie.mainActors} plot={movie.plot} metascore={movie.metascore} rating={movie.rating} />
+        <MovieCard key={movie.title+movie.director} {...movie} />
       ))}
     </div>
   );
